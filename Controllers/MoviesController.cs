c84dc771fc34a80dad8820a4088488cf2be89a88
yigitkalan <sametykalan@gmail.com>
2023-11-25 13:29:48 +0300

    add search and filter functionality

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,11 +20,37 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            return _context.Movies != null ?
-                        View(await _context.Movies.ToListAsync()) :
-                        Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            if (_context.Movies == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movies
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movies
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
---
+++ Models/MovieGenreViewModel.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models;
+public class MovieGenreViewModel {
+    public List<Movie> Movies { get; set; } = default!;
+    public SelectList Genres { get; set; } = default!;
+    public string MovieGenre { get; set; } = default!;
+    public string SearchString { get; set; } = default!;    
+
+}
